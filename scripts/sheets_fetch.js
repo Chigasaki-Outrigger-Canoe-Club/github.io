961d6f860dc44865b8a389cddac5298ea6cd43ce
ChigasakiOutriggerCanoeClub <manato.inokuchi@gmail.com>
2026-09-24 17:38:55 +0900

    Update sheets_fetch.js

--- scripts/sheets_fetch.js
+++ scripts/sheets_fetch.js
@@ -6,7 +6,7 @@ const COLUMN_MAP = {
   "記事タイトル": "title",
   "作成日": "date",
   "画像url(複数可)": "image_urls",
-  "本文URL(Google notes)": "body_doc_url",
+  "本文URL(Google Doc)": "body_doc_url",
   "notes": "category",
   "公開する?": "status",
   "生成済?": "generated",
